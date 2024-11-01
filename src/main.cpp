6fa0789bcf64db784a0bdf56a3902b6434481aa3
Maxim Uvarenkov <maxim.uvarenkov@gs-labs.ru>
2024-11-01 18:23:03 +0300

    [DEV] Logger notes.

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,15 @@
 
 //git config --add --local core.sshCommand 'ssh -i [SECRET_SSH_KEY_PATH]'
 
+//Logger notes
+//https://github.com/log4cplus/log4cplus
+//https://stackoverflow.com/questions/6692238/better-logging-library-for-c
+
+//I don't like to put Boost in all the projects.
 
 int main() {
 
 	std::cout << "Hello, OTUS World!" << std::endl;
-
 	std::cout << "Version is: " << otus_hw::VersionProvider::GetInstance().GetVersion() << std::endl;
 
 	return 0;
