28e9cb0c2bf89de58843d75262bfddd05f6fb9f4
Maxim Uvarenkov <maxim.uvarenkov@gs-labs.ru>
2024-11-05 11:33:54 +0300

    [DEV] Removing comments

--- src/templates/singleton/Singleton.h
+++ src/templates/singleton/Singleton.h
@@ -2,28 +2,6 @@
 #include <atomic>
 #include <iostream>
 
-
-// template <typename T>
-// class Singleton
-// {
-//     friend T;
-// public:
-//     static T& instance();
-
-// private:
-//     Singleton() = default;
-//     ~Singleton() = default;
-//     Singleton( const Singleton& ) = delete;
-//     Singleton& operator=( const Singleton& ) = delete;
-// };
-
-// template <typename T>
-// T& Singleton<T>::instance()
-// {
-//     static T inst;
-//     return inst;
-// }
-
 //https://stackoverflow.com/questions/41328038/singleton-template-as-base-class-in-c
 
 template <typename T>
@@ -32,7 +10,6 @@ class Singleton {
 public:
 	static T& GetInstance(){
 		std::call_once(initInstanceFlag, &Singleton::initSingleton);
-		// volatile int dummy{};
 		return *instance;
 	}
 private:
